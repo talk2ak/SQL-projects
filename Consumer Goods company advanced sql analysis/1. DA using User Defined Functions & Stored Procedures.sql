146ac5416cdcf388fe631bf167ec7f63fa14a80f
Akhilesh <104788315+talk2ak@users.noreply.github.com>
2023-08-12 15:17:41 +0530

    Update 1. DA using User Defined Functions & Stored Procedures.sql

--- Consumer Goods company advanced sql analysis/1. DA using User Defined Functions & Stored Procedures.sql
+++ Consumer Goods company advanced sql analysis/1. DA using User Defined Functions & Stored Procedures.sql
@@ -1,33 +1,32 @@
 
-/*  For a Consumer Goods company, Sales Data Analysis was performed using User Defined Function and Stored Procedures.
+ ########### Sales Data Analysis using User Defined Function and Stored Procedures  ############
 
-Problem 1:
+/* 
+Problem: There is a Consumer Goods company who has global presence. They want to analyse Sales data for thier Croma India customer (cust_code = 90002002),
+	 on Fiscal year basis (Starts in September).
 
-Here, an Aggregate monthly sales report is generated for croma india customer (customer_code = 90002002). so that we can track how much sales 
-this particular customer is generating.
+Approach: 
+1. A User defined function is created to generate Fiscal Year based Sales report for Croma India. (on Month and Year basis )	 
 
-Task 1: The report fields 1. Month, 2. Product name, 3. Variant, 4. Sold Quantity, 5. Gross price per Item, 6. Gross price Total 
+2. A Stored Procedure is created to generate Fiscal Year based Sales report with customer code as parameter.
 
-Task 2: The report fields: 1. Month, 2. Total gross sales amount to croma india this month.
-
+3. A Stored Procedure is created to generate Fiscal Year based Sales report report where Multiple customers can be parameter.
+	  
+ 4. A Stored Procedure is created to generate Market Badge (Gold, Silver) having following logic,
+	 If total sold quantity > 5 million that market is considered gold else silver.
 */
 
-###### USER DEFINED FUNCTION ##########
+	 
+############################## 1. USER DEFINED FUNCTION for Fiscal Year ################################
 
+
+# Getting Record for Croma, India customer (Normal Query)
 select distinct * 
 from dim_customer
 where customer like '%croma%' and market ='india';
 
 
--- Getting fiscal year calculation. 
-select * 
-from fact_sales_monthly
-where customer_code = 90002002 and 
-year(date_add(date, interval 4 month)) = 2021
-order by date desc;
-
-
-# Now we will create a function to get fiscal year date
+# Creating a function to get fiscal year date (FY starts from September)
 
 delimiter &&
 create function get_fiscal_year(calender_date date) 		
@@ -42,52 +41,9 @@ end &&
 delimiter ;
 
 
-# Let us use above created function in our query
 
-select * 
-from fact_sales_monthly
-where customer_code = 90002002 and 
-get_fiscal_year(date) = 2021         
-order by date asc;
-
-
-
-# Similarly, Writing a Quarter user defined function -
-
-delimiter &&
-create function get_fiscal_quarter( calender_date date)
-returns char(2)     
-deterministic
-begin
-	declare m tinyint;      		
-    declare qtr char(2);
-	set m = month(calender_date);	
-
-	case
-		when m in (9,10,11) then set qtr = "Q1";	
-		when m in (12,1,2) then set qtr = "Q2";
-		when m in (3,4,5) then set qtr = "Q3";
-		else 
-			set qtr = "Q4";
-        
-    end case ;				
-
-return qtr;
-end &&
-delimiter ;
-
-
-
-# Now using the above get_fiscal_quarter function as a condition to further refine search
-
-select * 
-from fact_sales_monthly
-where customer_code = 90002002 and 
-get_fiscal_year(date) = 2021 and get_fiscal_quarter(date) = "q4"
-order by date desc;
-
-
-# getting details from product table using above functions
+## Using get_fiscal_year user defined function to generate --> Month wise report for Croma India having following columns:
+-- Date, Product code, Product, Variant, Sold Quantity, Gross Price and Total Gross Price
 
 select 
 	s.date, s.product_code, product, variant, s.sold_quantity, 
@@ -105,30 +61,7 @@ limit 100;
 
 
 
-
-/* Problem:2 -
-As a product owner, I need an aggregate monthly sales report for croma india customer so that i can track how much sales this particular customer is generating for 
-AtliQ and manage our relationship accordingly.
-The report should have following fields:
-1. Month
-2. Total gross sales amount to croma india this month.
-
-*/
-
-select 
-	s.date, 
-	round(sum(g.gross_price*sold_quantity), 2) as gross_price_total
-from fact_sales_monthly s
-join fact_gross_price g
-on
-	g.product_code = s.product_code and
-    g.fiscal_year = get_fiscal_year(s.date)  
-where customer_code = 90002002				
-group by s.date
-order by s.date asc;
-
-
-# Yearly Sales Report --> Yearly sales report for Croma India where there are two columns:
+### Using get_fiscal_year user defined function to generate --> Yearly sales report for Croma India where there are two columns:
 -- 1. Fiscal Year
 -- 2. Total Gross Sales amount In that year from Croma
 
@@ -148,9 +81,9 @@ order by fiscal_year_re asc;
 
 
 
-############################### STORED PROCEDURES ##################################
+############################### 2. CREATING STORED PROCEDURES for a given customer on monthly basis  ##################################
 
--- Writing a stored procedure for getting monthly gross sale for a given customer
+## Writing a stored procedure for getting yearly gross sale for a given customer
 
 delimiter $$
 use gdb0041 $$
@@ -177,15 +110,11 @@ delimiter ;
 call gdb0041.get_monthly_gross_sale_for_customer(90002002);
 
 
--- Now, we can extract any data for a given customer code and checking it.
-
-select *
-from dim_customer
-where customer like '%amazon%' and market = 'india';
 
 
 
--- now lets create the stored procedure- which can take multiple customer codes
+############################### 3. CREATING STORED PROCEDURES for a Multiple customer on monthly basis  ##################################
+
 use gdb0041;
 drop procedure if exists get_monthly_gross_sale_for_customer;
 
@@ -212,32 +141,19 @@ end $$
 delimiter ;
 
 
--- lets call the above created procedure for multiple customer codes like in the case of amazon.
-
+-- lets call the above created Stored procedure for multiple customers
 call get_monthly_gross_sale_for_customer('90002008,90002016');   
 
 
 
-/* Problem: 3-
-Create a stored procedure that can determine the market badge based on following logic,
-If total sold quantity > 5 million that market is considered gold else silver
-My input -> 1. Market, 2. fiscal year
-Output-> market badge
 
-*/
 
--- First lets write a select query to get total sold quantity for 2021 fiscal year and country india
+############################### 4. CREATING STORED PROCEDURES for Market Badge (Gold & Silver)  ##################################
+
+
+# First lets write a select query to get total sold quantity for 2021 fiscal year and country india
+
 select c.market,
-	sum(sold_quantity) as total_qty
-from fact_sales_monthly s 
-join dim_customer c
-on 
-	s.customer_code = c.customer_code
-where get_fiscal_year(s.date) = 2021 
-group by c.market;
-
-
-select 
 	sum(sold_quantity) as total_qty
 from fact_sales_monthly s 
 join dim_customer c
@@ -248,7 +164,7 @@ group by c.market;
 
 
 
-# Now creating the stored procedure 
+## Creating the stored procedure for Market Badge; Condition --> total sold quantity > 5 million that market is considered gold else silver 
 
 delimiter &&
 create procedure get_market_badge( 
@@ -290,6 +206,41 @@ set @out_badge = 0;
 call get_market_badge('india',2020,@out_badge);
 select @out_badge;
 
+
+=====================================================================================================================================
+
+# Other useful Queries
+
+
+# Writing a Quarter user defined function -
+delimiter &&
+create function get_fiscal_quarter( calender_date date)
+returns char(2)     
+deterministic
+begin
+	declare m tinyint;      		
+    declare qtr char(2);
+	set m = month(calender_date);	
+
+	case
+		when m in (9,10,11) then set qtr = "Q1";	
+		when m in (12,1,2) then set qtr = "Q2";
+		when m in (3,4,5) then set qtr = "Q3";
+		else 
+			set qtr = "Q4";
+        
+    end case ;				
+
+return qtr;
+end &&
+delimiter ;
+
+# Now using the above get_fiscal_quarter function as a condition to further refine search
+select * 
+from fact_sales_monthly
+where customer_code = 90002002 and 
+get_fiscal_year(date) = 2021 and get_fiscal_quarter(date) = "q4"
+order by date desc;
 
 
 
